673d89d3c9d163f636a850865f23f2214fb74efd
Kwadwo Adu Nsiah Ahenkan <>
2026-05-24 23:31:21 +0000

    update

--- scripts/place.js
+++ scripts/place.js
@@ -10,12 +10,7 @@ const temperature = 10;
 const windSpeed = 5;
 
 function calculateWindChill(temp, speed) {
-    return (
-        13.12 +
-        0.6215 * temp -
-        11.37 * Math.pow(speed, 0.16) +
-        0.3965 * temp * Math.pow(speed, 0.16)
-    ).toFixed(1);
+    return (13.12 + 0.6215 * temp - 11.37 * Math.pow(speed, 0.16) + 0.3965 * temp * Math.pow(speed, 0.16)).toFixed(1);
 }
 
 const windChill = document.querySelector("#windchill");
